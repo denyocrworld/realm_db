0f884bd0161aecb9181c957e02ccf549a94da8e5
deny <deny@google.com>
2023-06-12 11:18:11 +0700

    .

---
+++ bin/realm_db.dart
@@ -0,0 +1,5 @@
+import 'package:realm_db/realm_db.dart' as realm_db;
+
+void main(List<String> arguments) {
+  print('Hello world: ${realm_db.calculate()}!');
+}
---
+++ test/realm_db_test.dart
@@ -0,0 +1,8 @@
+import 'package:realm_db/realm_db.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
